4c1da6e212937649de77c77521d06fd34f16b806
alancheg <chenggui53@gmail.com>
2017-07-13 13:01:07 +0800

    mr 树 暂时有一些问题，先处理 pksr 树

--- cr_and_mr/mr_tree.py
+++ cr_and_mr/mr_tree.py
@@ -69,6 +69,14 @@ class Rtree(object):
         # def leaf_distance(center, leaf):
         #     return ((leaf.MBR['xmin'] - center.MBR['xmin']) ** 2 + (leaf.MBR['ymin'] - center.MBR['ymin']) ** 2) ** 0.5
 
+        def leaf_distance(center, leaf):
+            # 距离度量函数 v2 ，通过将距离换乘面积来减少时间复杂度
+            return abs((leaf.MBR['xmin'] - center.MBR['xmin']) * (leaf.MBR['ymin'] - center.MBR['ymin']))
+
+        def calc_mass_center(center, node):
+            # 增加点之后计算质心，返回新的质心
+            pass
+
         if self.father is None:
             # 父节点的层级比当前节点多1。
             self.father = Rtree(level = self.level + 1, m = self.m, M = self.M)
@@ -79,165 +87,211 @@ class Rtree(object):
         leaf2 = Rtree(level = self.level, m = self.m, M = self.M, father = self.father)
 
         # 调用PickSeeds为leaf1和leaf2分配子节点
-        self.PickSeeds(leaf1, leaf2)
+        # self.PickSeeds(leaf1, leaf2)
 
-        # 遍历剩余的子节点，进行插入。
-        while len(self.leaves) > 0:
-            # 如果剩余的子节点插入某一组才能使该组节点数大于m，则直接全部插入进去，并调整MBR。
-            if len(leaf1.leaves) > len(leaf2.leaves) and len(leaf2.leaves) + len(self.leaves) == self.m:
-                for leaf in self.leaves:
-                    leaf2.MBR = merge(leaf2.MBR, leaf.MBR)
-                    leaf2.leaves.append(leaf)
-                    leaf.father = leaf2
-                self.leaves = []
-                break
+        # # ---------------  todo： 矩阵法 暂时不适用 ------------------------- #
+        # # 构建一个距离矩阵，减少计算量
+        # # 构建的矩阵为一个上三角矩阵，
+        # # i = j 时 为 0 i < j 时为 -1
+        # # ---- 构建两个特殊的矩阵，从而减少时间复杂度 ---------#
+        # nearest_father = []
+        # distance_matrix = []
+        #
+        #
+        # for num_1 in range(len(self.leaves)):
+        #     now_list = []
+        #     for num_2 in range(len(self.leaves)):
+        #         if num_1 == num_2:
+        #             now_list.append(0)
+        #         elif num_1 > num_2:
+        #             now_list.append(leaf_distance(self.leaves[num_1], self.leaves[num_2]))
+        #         else:
+        #             now_list.append(-1)
+        #
+        # i = 0
+        # j = 0
+        #
+        # sum_distance = 0
+        # # 中心点判断函数，找出能够使得总体中心点距离最小的函数
+        # for i in range(len(self.leaves)):
+        #     for j in range(i+1, len(self.leaves)):
+        #         pass
+        #
+        # # ----------- end ---------- #
 
-            if len(leaf2.leaves) > len(leaf1.leaves) and len(leaf1.leaves) + len(self.leaves) == self.m:
-                for leaf in self.leaves:
-                    leaf1.MBR = merge(leaf1.MBR, leaf.MBR)
-                    leaf1.leaves.append(leaf)
-                    leaf.father = leaf1
-                self.leaves = []
-                break
 
-            # 否则调用PickNext为leaf1和leaf2分配下一个节点。
-            self.PickNext(leaf1, leaf2)
+        # # 遍历剩余的子节点，进行插入。
+        # while len(self.leaves) > 0:
+        #     # 如果剩余的子节点插入某一组才能使该组节点数大于m，则直接全部插入进去，并调整MBR。
+        #     if len(leaf1.leaves) > len(leaf2.leaves) and len(leaf2.leaves) + len(self.leaves) == self.m:
+        #         for leaf in self.leaves:
+        #             leaf2.MBR = merge(leaf2.MBR, leaf.MBR)
+        #             leaf2.leaves.append(leaf)
+        #             leaf.father = leaf2
+        #         self.leaves = []
+        #         break
+        #
+        #     if len(leaf2.leaves) > len(leaf1.leaves) and len(leaf1.leaves) + len(self.leaves) == self.m:
+        #         for leaf in self.leaves:
+        #             leaf1.MBR = merge(leaf1.MBR, leaf.MBR)
+        #             leaf1.leaves.append(leaf)
+        #             leaf.father = leaf1
+        #         self.leaves = []
+        #         break
+        #
+        #     # 否则调用PickNext为leaf1和leaf2分配下一个节点。
+        #     # self.PickNext(leaf1, leaf2)
 
         # 当前节点的父节点删除掉当前节点并加入新的两个节点，完成分裂。
+
+        # todo: 质心法，测试有效性 #
+        # 1. 随机找到两个初始的质心点
+        mass_center1 = random.randint(0,int(len(self.leaves)/2))
+        mass_center2 = random.randint(int(len(self.leaves)/2), len(self.leaves))
+
+        # 2. 求出使得距离最小的数据项
+        # choose_node = -1
+        node_list_1 = []
+        node_list_2 = []
+
+
+
+        # ---- end ------- #
+
         self.father.leaves.remove(self)
         self.father.leaves.append(leaf1)
         self.father.leaves.append(leaf2)
         self.father.MBR = merge(self.father.MBR, leaf1.MBR)
         self.father.MBR = merge(self.father.MBR, leaf2.MBR)
 
-    # PickSeeds为两组节点分配子节点。
-    # todo:增加质心属性，从而提高 R 树的分裂效率
-    def PickSeeds(self, leaf1, leaf2):
-        """
-        MR 树
-        此版本的改进是通过引入质心的属性来提高整个 R 树的节点分裂效率
-
-        """
-        # todo: 如何简化距离度量函数
-        # 减少平方和立方的数量应该可以提高算法的速度
-
-        # def leaf_distance(center, leaf):
-        #     # 距离度量函数 v1 ，多次的平方和开根号容易导致时间增加
-        #     return ((leaf.MBR['xmin'] - center.MBR['xmin']) ** 2 + (leaf.MBR['ymin'] - center.MBR['ymin']) ** 2) ** 0.5
-
-        def leaf_distance(center, leaf):
-            # 距离度量函数 v2 ，通过将距离换乘面积来减少时间复杂度
-            return abs((leaf.MBR['xmin'] - center.MBR['xmin']) * (leaf.MBR['ymin'] - center.MBR['ymin']))
-
-        d = 0
-        t1 = 0
-        t2 = 0
-
-        # # 遍历所有可能的子节点组合，寻找差值最大的项。
-        # for i in range(len(self.leaves)):
-        #     for j in range(i + 1, len(self.leaves)):
-        #         # -------------------------------------------------------- #
-        #         MBR_new = merge(self.leaves[i].MBR, self.leaves[j].MBR) # 合并两个 MBR
-        #         S_new = 1.0 * (MBR_new['xmax'] - MBR_new['xmin']) * (MBR_new['ymax'] - MBR_new['ymin'])
-        #
-        #         S1 = 1.0 * (self.leaves[i].MBR['xmax'] - self.leaves[i].MBR['xmin']) * (self.leaves[i].MBR['ymax'] - self.leaves[i].MBR['ymin'])
-        #         S2 = 1.0 * (self.leaves[j].MBR['xmax'] - self.leaves[j].MBR['xmin']) * (self.leaves[j].MBR['ymax'] - self.leaves[j].MBR['ymin'])
-        #
-        #         if S_new - S1 - S2 > d:
-        #             t1 = i
-        #             t2 = j
-        #             d = S_new - S1 - S2
-        #         # -------------------------------------------------------- #
-
-        # 计算出最小距离点
-        # 通过随机指定两个值，计算点的距离和，找出能够使总体距离和最小的值
-        for i in range(len(self.leaves)):
-            for j in range(i+1, len(self.leaves)):
-
-                sum_of_distance = 0
-                for k in range(len(self.leaves)):
-                    if k is not i and k is not j:
-                        sum_of_distance += min(leaf_distance(self.leaves[i], self.leaves[k]), leaf_distance(self.leaves[j], self.leaves[k]))
-
-                if sum_of_distance > d:
-                    d = sum_of_distance
-                    t1 = i
-                    t2 = j
-
-        # --------- 新的方法 ------------- #
-        # 找出数据中的质心，从而确定中心点
-        # data_list = []
-        #
-        #
-        # distance = 0
-        # addr = []
-        # for i in range(data_list):
-        #     for j in range(data_list):
-        #         pass
-
-        # --------- end ----------------- #
-
-        n2 = self.leaves.pop(t2)
-        n2.father = leaf1
-        leaf1.leaves.append(n2)
-        leaf1.MBR = leaf1.leaves[0].MBR
-
-        n1 = self.leaves.pop(t1)
-        n1.father = leaf2
-        leaf2.leaves.append(n1)
-        leaf2.MBR = leaf2.leaves[0].MBR
-
-    # PickNext为两组节点分配一个子节点。
-    def PickNext(self, leaf1, leaf2):
-        # 距离计算函数
-        def leaf_distance(center, leaf):
-            return ((leaf.MBR['xmin'] - center.MBR['xmin']) ** 2 + (leaf.MBR['ymin'] - center.MBR['ymin']) ** 2) ** 0.5
-
-        # # ------------------- old way -------- #
-        # d = 0
-        # t = 0
-        #
-        # # 遍历子节点，找到插入两组节点后面积增加差值最大的一项。
-        # for i in range(len(self.leaves)):
-        #     d1 = space_increase(merge(leaf1.MBR, self.leaves[i].MBR), leaf1.MBR)
-        #     d2 = space_increase(merge(leaf2.MBR, self.leaves[i].MBR), leaf2.MBR)
-        #     if abs(d1 - d2) > abs(d):
-        #         d = d1 - d2
-        #         t = i
-        #
-        # if d > 0:
-        #     target = self.leaves.pop(t)
-        #     leaf2.MBR = merge(leaf2.MBR, target.MBR)
-        #     target.father = leaf2
-        #     leaf2.leaves.append(target)
-        # else:
-        #     target = self.leaves.pop(t)
-        #     leaf1.MBR = merge(leaf1.MBR, target.MBR)
-        #     target.father = leaf1
-        #     leaf1.leaves.append(target)
-        # # ------------------ old way end --------- #
-
-        # 传统的方法是找出使得面积最大的子节点
-        # 新的方法是找出使得节点更加紧密的子节点
-        # ------------------- new way ------------ #
-        d = 0
-        t = 0
-
-        # 找到一个子节点，分配给一个离它最近的面积
-        if leaf_distance(leaf2, self.leaves[t]) > leaf_distance(leaf1, self.leaves[t]):
-            d = 1
-
-        if d > 0:
-            target = self.leaves.pop(t)
-            leaf2.MBR = merge(leaf2.MBR, target.MBR)
-            target.father = leaf2
-            leaf2.leaves.append(target)
-        else:
-            target = self.leaves.pop(t)
-            leaf1.MBR = merge(leaf1.MBR, target.MBR)
-            target.father = leaf1
-            leaf1.leaves.append(target)
-        # ------------------- new way end --------- #
+    # # PickSeeds为两组节点分配子节点。
+    # # todo:增加质心属性，从而提高 R 树的分裂效率
+    # def PickSeeds(self, leaf1, leaf2):
+    #     """
+    #     MR 树
+    #     此版本的改进是通过引入质心的属性来提高整个 R 树的节点分裂效率
+    #
+    #     """
+    #     # todo: 如何简化距离度量函数
+    #     # 减少平方和立方的数量应该可以提高算法的速度
+    #
+    #     # def leaf_distance(center, leaf):
+    #     #     # 距离度量函数 v1 ，多次的平方和开根号容易导致时间增加
+    #     #     return ((leaf.MBR['xmin'] - center.MBR['xmin']) ** 2 + (leaf.MBR['ymin'] - center.MBR['ymin']) ** 2) ** 0.5
+    #
+    #     def leaf_distance(center, leaf):
+    #         # 距离度量函数 v2 ，通过将距离换乘面积来减少时间复杂度
+    #         return abs((leaf.MBR['xmin'] - center.MBR['xmin']) * (leaf.MBR['ymin'] - center.MBR['ymin']))
+    #
+    #     d = 0
+    #     t1 = 0
+    #     t2 = 0
+    #
+    #     # # 遍历所有可能的子节点组合，寻找差值最大的项。
+    #     # for i in range(len(self.leaves)):
+    #     #     for j in range(i + 1, len(self.leaves)):
+    #     #         # -------------------------------------------------------- #
+    #     #         MBR_new = merge(self.leaves[i].MBR, self.leaves[j].MBR) # 合并两个 MBR
+    #     #         S_new = 1.0 * (MBR_new['xmax'] - MBR_new['xmin']) * (MBR_new['ymax'] - MBR_new['ymin'])
+    #     #
+    #     #         S1 = 1.0 * (self.leaves[i].MBR['xmax'] - self.leaves[i].MBR['xmin']) * (self.leaves[i].MBR['ymax'] - self.leaves[i].MBR['ymin'])
+    #     #         S2 = 1.0 * (self.leaves[j].MBR['xmax'] - self.leaves[j].MBR['xmin']) * (self.leaves[j].MBR['ymax'] - self.leaves[j].MBR['ymin'])
+    #     #
+    #     #         if S_new - S1 - S2 > d:
+    #     #             t1 = i
+    #     #             t2 = j
+    #     #             d = S_new - S1 - S2
+    #     #         # -------------------------------------------------------- #
+    #
+    #     # 计算出最小距离点
+    #     # 通过随机指定两个值，计算点的距离和，找出能够使总体距离和最小的值
+    #     for i in range(len(self.leaves)):
+    #         for j in range(i+1, len(self.leaves)):
+    #
+    #             sum_of_distance = 0
+    #             for k in range(len(self.leaves)):
+    #                 if k is not i and k is not j:
+    #                     sum_of_distance += min(leaf_distance(self.leaves[i], self.leaves[k]), leaf_distance(self.leaves[j], self.leaves[k]))
+    #
+    #             if sum_of_distance > d:
+    #                 d = sum_of_distance
+    #                 t1 = i
+    #                 t2 = j
+    #
+    #     # --------- 新的方法 ------------- #
+    #     # 找出数据中的质心，从而确定中心点
+    #     # data_list = []
+    #     #
+    #     #
+    #     # distance = 0
+    #     # addr = []
+    #     # for i in range(data_list):
+    #     #     for j in range(data_list):
+    #     #         pass
+    #
+    #     # --------- end ----------------- #
+    #
+    #     n2 = self.leaves.pop(t2)
+    #     n2.father = leaf1
+    #     leaf1.leaves.append(n2)
+    #     leaf1.MBR = leaf1.leaves[0].MBR
+    #
+    #     n1 = self.leaves.pop(t1)
+    #     n1.father = leaf2
+    #     leaf2.leaves.append(n1)
+    #     leaf2.MBR = leaf2.leaves[0].MBR
+    #
+    # # PickNext为两组节点分配一个子节点。
+    # def PickNext(self, leaf1, leaf2):
+    #     # 距离计算函数
+    #     def leaf_distance(center, leaf):
+    #         return ((leaf.MBR['xmin'] - center.MBR['xmin']) ** 2 + (leaf.MBR['ymin'] - center.MBR['ymin']) ** 2) ** 0.5
+    #
+    #     # # ------------------- old way -------- #
+    #     # d = 0
+    #     # t = 0
+    #     #
+    #     # # 遍历子节点，找到插入两组节点后面积增加差值最大的一项。
+    #     # for i in range(len(self.leaves)):
+    #     #     d1 = space_increase(merge(leaf1.MBR, self.leaves[i].MBR), leaf1.MBR)
+    #     #     d2 = space_increase(merge(leaf2.MBR, self.leaves[i].MBR), leaf2.MBR)
+    #     #     if abs(d1 - d2) > abs(d):
+    #     #         d = d1 - d2
+    #     #         t = i
+    #     #
+    #     # if d > 0:
+    #     #     target = self.leaves.pop(t)
+    #     #     leaf2.MBR = merge(leaf2.MBR, target.MBR)
+    #     #     target.father = leaf2
+    #     #     leaf2.leaves.append(target)
+    #     # else:
+    #     #     target = self.leaves.pop(t)
+    #     #     leaf1.MBR = merge(leaf1.MBR, target.MBR)
+    #     #     target.father = leaf1
+    #     #     leaf1.leaves.append(target)
+    #     # # ------------------ old way end --------- #
+    #
+    #     # 传统的方法是找出使得面积最大的子节点
+    #     # 新的方法是找出使得节点更加紧密的子节点
+    #     # ------------------- new way ------------ #
+    #     d = 0
+    #     t = 0
+    #
+    #     # 找到一个子节点，分配给一个离它最近的面积
+    #     if leaf_distance(leaf2, self.leaves[t]) > leaf_distance(leaf1, self.leaves[t]):
+    #         d = 1
+    #
+    #     if d > 0:
+    #         target = self.leaves.pop(t)
+    #         leaf2.MBR = merge(leaf2.MBR, target.MBR)
+    #         target.father = leaf2
+    #         leaf2.leaves.append(target)
+    #     else:
+    #         target = self.leaves.pop(t)
+    #         leaf1.MBR = merge(leaf1.MBR, target.MBR)
+    #         target.father = leaf1
+    #         leaf1.leaves.append(target)
+    #     # ------------------- new way end --------- #
 
 
     # AdjustTree自底向上调整R树。
